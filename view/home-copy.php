27d4e81282a6b2adbe4e0dde809a05c3543e1896
Chafiahmdi <chabxl2022@icloud.com>
2023-10-04 11:15:13 +0200

    bleu, animation et footer

--- view/home-copy.php
+++ view/home-copy.php
@@ -38,7 +38,7 @@
                 }
 
                 body{
-                  @apply text-gray-500;
+                  @apply text-gray-500 relative;
                 }
                
                 html {
@@ -64,6 +64,10 @@
                 .app-short-desc{
                     @apply shadow-xl;
                 }
+                .carte{
+                  @apply h-96 w-80 my-14 bg-white rounded-3xl;
+                }
+                 
                 
         }
           </style>
@@ -147,10 +151,47 @@
 
             <img class=" mb-6 ml-auto mr-auto w-80 md:w-96" src="../assets/images/logo-colok.svg" alt="logo"> 
             <p class="mx-auto w-[11.2rem] text-black relative md:w-full mt-6 mb-12"> Des tâches partagées, une colocation équilibrée </p>
-            <div class="illust-propos flex flex-col h-24 items-center justify-between mt-28	md:flex-row md:justify-center">
-                <button class="bg-white md:mr-3"><a href="signIn.php">Créer un compte</a></button>
-                <button  class="bg-[#257EA7] text-white md:ml-3"><a href="signIn.php">Se connecter</a></button>
-            </div> 
+
+
+<div class="h-auto w-auto flex flex-col justify-evenly items-center  md:flex-row w-1/2 flex-wrap	">
+
+<div class="carte">
+  <div class="px-8 rounded-lg h-64 w-full text-justify px-4 pb-2 pt-8">
+    <img src="../assets/images/profil.svg" alt="" class="w-24 h-24 m-auto mb-6">
+
+    <h3>Profil colocataire</h3>
+    <p>Découvrez vos futurs colocataires. Créez un profil unique pour mieux vous connaître et trouver la colocation parfaite.</p>
+  </div>
+</div>
+
+<div class="carte">
+  
+  <div class="px-8  h-64 w-full text-justify px-4 py-2 pt-8">
+    <img src="../assets/images/calendar.svg" alt="" class="w-24 h-24 m-auto mb-6">
+    <h3>Planning des tâches</h3>
+    <p>Restez organisé et équitable. Planifiez et suivez les tâches ménagères et les responsabilités de manière transparente.</p>
+  </div>
+</div>
+
+<div class="carte">
+  
+  <div class="px-8 rounded-lg h-64 w-full text-justify px-4 py-2 pt-8">
+    <img src="../assets/images/notif.svg" alt="" class="w-24 h-24 m-auto mb-6">
+    <h3>Notifications</h3>
+    <p>Restez informé et connecté. Recevez des notifications en temps réel sur les événements importants, les rappels de tâches et les mises à jour essentielles pour une colocation harmonieuse.</p>
+  </div>
+</div>
+
+<div class="carte">
+  
+  <div class="px-8 rounded-lg h-64 w-full text-justify px-4 py-2 pt-8">
+    <img src="../assets/images/box.svg" alt="" class="w-24 h-24 m-auto mb-6">
+    <h3>Boîte à idées</h3>
+    <p>Partagez vos idées, améliorez votre colocation. Proposez des suggestions et contribuez à rendre votre
+      maison encore meilleure.</p>
+  </div>
+</div>
+
             <div   class=" absolute inset-x-0 bottom-0" ><img src="../assets/images/maisons.svg" alt="image de maisons"></div>           
         </div> 
 <!--#endregion -->        
@@ -284,14 +325,13 @@
     </main>
 
 
-     
-<footer class=" bg-[#257EA7]">
+<footer class="bg-[#257EA7] absolute inset-x-0 bottom-0">
         <!-- This is an example component -->
-    <div>
+    <div class="">
         <div class="max-w-2xl mx-auto bg-[#257EA7] text-white  py-10">
             <div class="text-center">
                 <h3 class="text-3xl mb-3">Téléchargez notre application de gestion des tâches ménagères </h3>
-                <p> Des tâches partagées, une colocation équilibrée. </p>
+                <p> Rester en forme. Toute la journée, tous les jours. </p>
                 <div class="flex justify-center my-10">
                     <div class="flex items-center border w-auto rounded-lg px-4 py-2 w-52 mx-2">
                         <img src="https://cdn-icons-png.flaticon.com/512/888/888857.png" class="w-7 md:w-8">

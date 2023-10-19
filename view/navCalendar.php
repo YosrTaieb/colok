ba96e49633c73508dcf7476cf1c87f65926d49ba
YosrTaieb <yosr.taieb@gmail.com>
2023-10-19 19:30:00 +0200

    rectification de la navCalendar

--- view/navCalendar.php
+++ view/navCalendar.php
@@ -14,16 +14,28 @@
     <i class="fas fa-bars"></i>
     </label>
     <ul>
-      <li> <a class="active" href="">Boîte à idées</a></li>
-      <li> <a href="">Planning</a></li>
+      <li>
+        <a href="../view/form-idees.php"><i class="fas fa-lightbulb"></i>Boîte à idées
+        </a>
+      </li>
+      <li>
+        <a href="calendrier.php"><i class="far fa-calendar-alt"></i>Planning
+        </a>
+      </li>
     </ul>
     <div class="nav-img">
       <img src="../assets/images/logo-colok.svg" alt="">
     </div>
     <!-- <label class="logo" for="">Colok</label> -->
     <ul>
-      <li><a href="">Inscription</a></li>
-      <li><a href="">Connexion</a></li>
+      <li>
+        <a href="../view/mesTaches.php"><i class="fas fa-tasks"></i>Mes Tâches
+        </a>
+      </li>
+      <li>
+        <a href="../view/forms.php"><i class="fas fa-home"></i>Profil Maison
+        </a>
+      </li>
     </ul>
   </nav>
 </body>

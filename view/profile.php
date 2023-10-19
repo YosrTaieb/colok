df84fac406606f8690478b77b99148a5e3b2360f
YosrTaieb <yosr.taieb@gmail.com>
2023-10-19 17:47:54 +0200

    changement du layout de la page profile

--- view/profile.php
+++ view/profile.php
@@ -15,53 +15,22 @@
     <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <script src="../js/app.js"defer></script>
-    <!-- <scrip src="../js/forms.js" defer></script> -->
     <link rel="stylesheet" href="../build/styles/nav.css" />
     <link rel="stylesheet" href="../build/styles/footer.css" />
-
     <title>Profil utilisateur</title>
-    
   </head>
-  
+ 
   <body>
    <header class="w-full text-gray-700 bg-white sticky z-50 top-0 drop-shadow-md">
      <?php include("nav.php")?>
    </header>
 
-    
-
-    
-    
- 
   
-<!--   
 <div class="form-wrapper">
-<div class="form-illustration">
-    
-            <img src="../assets/images/illust-profil.gif" alt="">
-
-   
-  
-</div> -->
 <form style="display: none;"  action="../controller/profileControlleur.php" method="post" class="form mt-12" enctype="multipart/form-data">
 <a href="#" id="closeFormButton" class="close-button">
       <i class="fas fa-times"></i>
     </a>
-    <style>
-        /* Styles pour le bouton de fermeture */
-.close-button {
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  cursor: pointer;
-}
-
-/* Styles pour l'icône de fermeture */
-.close-button i {
-  margin-left: 900px; /* Espace entre l'icône et le texte */
-}
-
-    </style>
 <h1 class="page-title">Mon profil</h1>
     <div class="profile-picture">  
     <a href="#imageFile" onclick="poutpout()" class="profile-link">
@@ -102,15 +71,18 @@
                     <input type="email" name="email" id="email" value="<?php echo $user['email'] ?>"  required placeholder="Votre adresse e-mail" />
                 </div>
 
-                <div class="input-group">
-  <label for="mdp"><i class="fas fa-lock"></i>Mot de passe</label>
-  <input type="password" name="mdp" value="<?php echo $user['mdp'] ?>" id="mdp" placeholder="Votre mot de passe" />
-  <span class="password-toggle">
-    <i id="togglePassword" class="fas fa-eye"></i>
-  </span>
-</div>
 
-               
+<div class="input-group">
+                <div class="main-password">
+                  
+  <label for="mdp"><i class="fas fa-lock"></i>Mot de passe</label>
+
+  <input class="form-control input-password" type="password" name="mdp" value="<?php echo $user['mdp'] ?>" id="mdp" placeholder="Votre mot de passe" />
+  <a class="icon-view">
+  <i class="fas fa-eye"></i>
+  </a>
+</div>
+</div>               
 
                 <div class="input-group">
                     <label for="allergies"><i class="fas fa-allergies"></i>Allergies</label>
@@ -121,54 +93,40 @@
                 <input type="hidden" name='photo' value="<?php echo $user['photo_profil'] ?>">
                 <input type="file" id="imageFile"  name="fileToUpload" accept="image/*" style="display: none;">
                 </div>
-
-            
-               
-
                 <div class="btn-center">
                     <button class="form-btn" type="submit" name="submit">Valider</button>
                 </div>
             </div>
         </div>
     </div>
+        </div>
 </form>
 
-<div class="max-w-24 bg-white shadow-xl rounded-xl overflow-hidden my-8" id="card">
-
-
-
-<div class="max-w-24 bg-white shadow-xl rounded-xl overflow-hidden my-8" id="card">
-
-  <button class="bg-blue-400 hover:bg-blue-200 text-white font-bold py-2 px-8 rounded" id="editButton">
-    Edit
-  </button>
-
-  <img class="mx-auto my-4 xl py-2 px-4 rounded " src="../images/<?php echo $user['photo_profil'] ?>" alt="">
-
-  <!-- Le reste du contenu -->
-
+<div class="card-container" id="card">
+<figure class="snip1336">
+  <img src="../assets/images/house-illustration.jpeg" alt="sample87" />
+  <figcaption>
+    <img src="../images/<?php echo $user['photo_profil'] ?>" alt="profile-sample4" class="profile" />
+    <h2><?php echo $user['prenom'] ?> <?php echo $user['nom'] ?></h2>
+  
+    <div>
+    <p>
+      <i class="fas fa-birthday-cake text-2xl mr-2"></i>
+      <?php echo $user['date_naissance'] ?>
+   </p>
+   <p>
+   <i class="fas fa-allergies text-2xl mr-2"></i>
+      <?php echo $user['allergies'] ?>
+   </p>
+   <p>
+   <i class="fas fa-envelope text-2xl mr-2"></i>
+   <?php echo $user['email'] ?>
+   </p></div>
+    <a href="#" class="follow" id="editButton">Modifier</a>
+  </figcaption>
+</figure>
 </div>
 
-        <div class="py-8 px-24">
-            <h1 class="text-2xl font-semibold text-gray-800"><?php echo $user['nom'] ?></h1>
-            <p class="py-2 text-lg text-gray-700"><?php echo $user['prenom'] ?></p>
-            <div class="flex items-center mt-8 text-gray-700">
-            <i class="fas fa-birthday-cake text-2xl mr-2"></i>
-                <h1 class="px-2 text-sm"><?php echo $user['date_naissance'] ?></h1>
-            </div>
-            <div class="flex items-center mt-8 text-gray-700">
-            <i class="fas fa-allergies text-2xl mr-2"></i>
-                <h1 class="px-2 text-sm"><?php echo $user['allergies'] ?></h1>
-            </div>
-            <div class="flex items-center mt-8 text-gray-700">
-            <i class="fas fa-envelope text-2xl mr-2"></i>
-                <h1 class="px-2 text-sm"><?php echo $user['email'] ?></h1>
-            </div>
-        </div>
-    </div>
-
-</div>
-[11:06] Safae Ben Attou
 
 <script>
 

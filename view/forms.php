706e006fcbd367cfdea21d0f482cf4a78576e8e6
safaebn3 <safabenattou3@gmail.com>
2023-10-19 10:37:52 +0200

    accepte change

--- view/forms.php
+++ view/forms.php
@@ -7,15 +7,11 @@
     <link rel="stylesheet" href="../build/styles/forms.css" />
     <script src="../js/forms.js" defer></script>
     <link rel="stylesheet" href="../build/styles/nav.css" />
-<<<<<<< HEAD
-    <link rel="stylesheet" href="../build/styles/footer.css" />
-=======
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.0/css/all.min.css">
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" defer></script>
     <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
->>>>>>> b8cd1a8 (update header)
     <script src="../js/app.js"defer></script>
     <script src="https://cdn.tailwindcss.com"></script>
     <title>Formulaire d'inscription</title>

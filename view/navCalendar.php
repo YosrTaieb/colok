37d87ddac82b8eeacb6db7c2acd456bdb32c6cc2
YosrTaieb <yosr.taieb@gmail.com>
2023-10-20 10:01:04 +0200

    layout calendar

--- view/navCalendar.php
+++ view/navCalendar.php
@@ -1,13 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-  <link rel="stylesheet" href="../build/styles/navCalendar.css">
-  <script src="https://kit.fontawesome.com/3b1384a103.js" crossorigin="anonymous"></script>
-</head>
-<body>
+
+  <header>
   <nav>
     <input type="checkbox" id="check">
     <label for="check" class="checkbtn">
@@ -38,5 +30,4 @@
       </li>
     </ul>
   </nav>
-</body>
-</html>
+  </header>
